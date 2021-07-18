1c1b7299d43273d6b7cea7fd5df86e340f743777
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2021-07-18 16:09:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('Alien-Alfa\nBy Sfwan/Toxic-Alien\nv2.0.0', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
